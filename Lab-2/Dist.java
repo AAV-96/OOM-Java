baa757dcd0080f19e8d6211a2cf2213b43ff8ce8
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2016-09-05 13:38:01 +0530

    Add files via upload

---
+++ Lab-2/Dist.java
@@ -0,0 +1,16 @@
+import user.own.pack.Distance;
+import java.io.*;
+import java.util.*;
+
+class Dist {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		Distance obj1, obj2;
+		obj1 = new Distance(0);
+		obj2 = new Distance(0);
+		Distance obj3 = obj1.getsum(obj2);
+
+		System.out.println("Sum of feet = "+obj3.getft());
+		System.out.println("Sum of Inches = "+obj3.getin());
+	}
+}
