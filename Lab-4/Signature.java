919dcbf1c93e306beb271589e7ab4a276d59096f
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2016-09-20 00:21:43 +0530

    Add files via upload

---
+++ Lab-4/Signature.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Signature {
+	public static void main(String args[]) {
+		System.out.println("Name: Ashwin A. Vardhan");
+		System.out.println("Email: iit2015104@iiita.ac.in");
+		System.out.println("Ph No.: 08604616966");
+		
+		System.out.println("Arg1: "+args[0]);
+		System.out.println("Arg2: "+args[1]);
+		System.out.println("Arg3: "+args[2]);
+	}
+}
