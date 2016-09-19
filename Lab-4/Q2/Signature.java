3ebacf95588b31a1f47354c1f0801e1e3e6e8a90
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2016-09-20 00:24:16 +0530

    Update and rename Lab-4/Signature.java to Lab-4/Q2/Signature.java

--- Lab-4/Q2/Signature.java
+++ Lab-4/Q2/Signature.java
@@ -10,5 +10,9 @@ public class Signature {
 		System.out.println("Arg1: "+args[0]);
 		System.out.println("Arg2: "+args[1]);
 		System.out.println("Arg3: "+args[2]);
+		/*Instead of directly using args[0] etc., we can use a for loop too.
+		for (int i=0; i<args.length; i++) {
+			System.out.println("Arg"+(i+1)+" "+args[i]);
+		}
 	}
 }
